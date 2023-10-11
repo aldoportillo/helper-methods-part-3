63e5109ef2ed1b2d577e825c957ac13545cf143c
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-11 21:50:09 +0000

    Use before_action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,8 @@
 class MoviesController < ApplicationController
+
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+
   def new
     @movie = Movie.new
   end
@@ -14,7 +18,6 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
@@ -31,11 +34,9 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
     
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
@@ -45,7 +46,6 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
 
     @movie.destroy
 
@@ -56,5 +56,9 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :description, :image_url, :released_on)
+  end
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 end
